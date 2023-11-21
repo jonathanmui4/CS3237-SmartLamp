c88a62f5fb87cba753172646daf268830144e930
Joel Wong <97379359+Jo3LW@users.noreply.github.com>
2023-11-21 23:29:29 +0800

    Update AWSImageDecoder.py

--- ML/AWS_production/AWSImageDecoder.py
+++ ML/AWS_production/AWSImageDecoder.py
@@ -5,6 +5,22 @@ from datetime import datetime
 
 s3 = boto3.client('s3')
 
+def is_valid_image(data):
+    # JPEG file signature (magic number)
+    jpeg_signature = b'\xFF\xD8\xFF\xE0'
+
+    try:
+        # Check if the first 4 bytes match the JPEG signature
+        if data[:4] == jpeg_signature:
+            return True
+        else:
+            print('Invalid image: Not a JPEG file')
+            return False
+    except Exception as e:
+        print(f'Error checking image format: {str(e)}')
+        return False
+
+
 def lambda_handler(event, context):
     try:
         # Extract the message payload from the MQTT event
@@ -12,11 +28,17 @@ def lambda_handler(event, context):
         
         # Get the base64 encoded message from the payload
         encoded_message = mqtt_payload['message']
-        print(encoded_message)
-        
+
         # Decode the base64 message to bytes
         decoded_bytes = base64.b64decode(encoded_message)
         print("Decoded Message received.")
+
+        # Check if the decoded bytes represent a valid image
+        if not is_valid_image(decoded_bytes):
+            return {
+                'statusCode': 400,
+                'body': 'Invalid image format'
+            }
 
         # Create a unique filename based on the current timestamp
         current_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
@@ -42,4 +64,3 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': f'Error: {str(e)}'
         }
-
